928d18c9e2e92b4612c1359372181434a39bccd6
muralievv <nmuraliev@gmail.com>
2026-01-16 17:22:01 +0600

    Hm2

--- blog/urls.py
+++ blog/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import product.views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', product.views.home),
     path('offers/', product.views.offers_list),
     path('offers/<int:product_id>/', product.views.offer_detail),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- product/models.py
+++ product/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+    
+    def __str__(self):
+        return f"{self.name}"
+
 class Product(models.Model):
-    photo = models.ImageField(null=True, blank=True)
+    photo = models.ImageField(null=True, blank=True, upload_to='product/')
     title = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
     salary = models.IntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
     
